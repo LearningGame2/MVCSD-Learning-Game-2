2d2cf6493a8e4abd337a5c7a2cb069a7af0654ec
srabieh <rabieh1@kenyon.edu>
2022-04-13 22:30:45 -0400

    practicing w/ variables

--- Test/Attempt3/Game.php
+++ Test/Attempt3/Game.php
@@ -24,7 +24,15 @@ if (mysqli_connect_errno()) {
     $sql = "SELECT * FROM QuestionDatabase WHERE QuestionNumber = 3";
     if ($result = mysqli_query($conn, $sql)) {
         while ($row = mysqli_fetch_row($result)) {
-          printf ("%s (%s) (%s)\n", $row[0], $row[1], $row[3]);
+          $prompt = $row[1];
+          $corr = $row[2];
+          $wr1 = $row[3];
+          $wr2 = $row[4];
+          $wr3 = $row[5];
+          echo $prompt;
+          echo $corr;
+          echo $wr1;
+          //printf ("%s (%s) (%s)\n", $row[0], $row[1], $row[3]);
         }
         mysqli_free_result($result);
       }
