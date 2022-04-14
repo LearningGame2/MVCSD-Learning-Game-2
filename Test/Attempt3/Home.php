fc821652232b2003986370537ce592d223e064ba
MasonFishell <fishell1@kenyon.edu>
2022-04-14 07:53:01 -0400

    dsa

--- Test/Attempt3/Home.php
+++ Test/Attempt3/Home.php
@@ -10,11 +10,14 @@
     <h1>Logged in!!!</h1>
     <h2>Press button to play </h2>
     <button type ="submit" onclick = "goToGame()">Play</button>
-    
+    <button type ="submit" onclick = "logOut()">Log out</button>
 </body>
 <script>
 function goToGame(){
     window.location.href = "Game.php"
+}
+function logOut(){
+    window.location.href = "Login1.html"
 }
 
 </script>
