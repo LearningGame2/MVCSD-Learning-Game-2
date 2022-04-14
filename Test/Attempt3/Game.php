79f25ac26879b09d8704a49efd1c275d70490670
MasonFishell <fishell1@kenyon.edu>
2022-04-14 00:57:19 -0400

    fdsa

--- Test/Attempt3/Game.php
+++ Test/Attempt3/Game.php
@@ -1,50 +1,93 @@
-<?php
-$conn = mysqli_connect("localhost","fishell1","S219352","Game2");
 
-if (mysqli_connect_errno()) {
-    echo "Failed to connect to MySQL: " . mysqli_connect_error();
-    exit();
-  }
-?>
-
-
-
-<!DOCTYPE html>
-<html lang="en">
 <head>
-    <meta charset="UTF-8">
-    
-    <title>BigGamerGuy 
-    </title>
+  <link rel="stylesheet" href="Game.css">
 </head>
-<h1>Playing Game</h1>
-<body>
-    
-<?php
-    $sql = "SELECT * FROM QuestionDatabase WHERE QuestionNumber = 3";
-    if ($result = mysqli_query($conn, $sql)) {
-        while ($row = mysqli_fetch_row($result)) {
-          $prompt = $row[1];
-          $corr = $row[2];
-          $wr1 = $row[3];
-          $wr2 = $row[4];
-          $wr3 = $row[5];
-          echo $prompt , ' is prompt\n';
-          echo -e $corr , 'is corr\n';
-          echo -e $wr1 , 'is wr1';
-          //printf ("%s (%s) (%s)\n", $row[0], $row[1], $row[3]);
-        }
-        mysqli_free_result($result);
-      }
-      
-      mysqli_close($con);
-   ?>
+
+<body onload="NextQuestion(0)">
+  <h1 style="background-color:black; color:white; text-align:center; font-family:courier new; font-size:300%; line-height: 100px">
+    Learning Game 2
+  </h1>
+    <main>
+        <!-- creating a modal for when quiz ends -->
+        <div class="modal-container" id="score-modal">
+
+            <div class="modal-content-container">
+
+                <h1>Congratulations, Quiz Completed.</h1>
+
+                <div class="grade-details">
+                    <p>Attempts : 10</p>
+                    <p>Wrong Answers : <span id="wrong-answers"></span></p>
+                    <p>Right Answers : <span id="right-answers"></span></p>
+                    <p>Grade : <span id="grade-percentage"></span>%</p>
+                    <p ><span id="remarks"></span></p>
+                </div>
+
+                <div class="modal-button-container">
+                    <button onclick="closeScoreModal()">Continue</button>
+                </div>
+
+            </div>
+        </div>
+<!-- end of modal of quiz details-->
+
+        <div class="game-quiz-container" style = "position:relative; top:-75px">
+
+            <div class="game-details-container">
+                <h1>Score : <span id="player-score"></span> / 10</h1>
+                <h1> Question : <span id="question-number"></span> / 10</h1>
+            </div>
+
+            <div class="game-question-container">
+                <h1 id="display-question"></h1>
+            </div>
+
+            <div class="game-options-container">
+
+               <div class="modal-container" id="option-modal">
+
+                    <div class="modal-content-container">
+                         <h1>Please Pick An Option</h1>
+
+                         <div class="modal-button-container">
+                            <button onclick="closeOptionModal()">Continue</button>
+                        </div>
+
+                    </div>
+
+               </div>
+
+                <span>
+                    <input type="radio" id="option-one" name="option" class="radio" value="optionA" />
+                    <label for="option-one" class="option" id="option-one-label"></label>
+                </span>
 
 
-    
+                <span>
+                    <input type="radio" id="option-two" name="option" class="radio" value="optionB" />
+                    <label for="option-two" class="option" id="option-two-label"></label>
+                </span>
+
+
+                <span>
+                    <input type="radio" id="option-three" name="option" class="radio" value="optionC" />
+                    <label for="option-three" class="option" id="option-three-label"></label>
+                </span>
+
+
+                <span>
+                    <input type="radio" id="option-four" name="option" class="radio" value="optionD" />
+                    <label for="option-four" class="option" id="option-four-label"></label>
+                </span>
+
+
+            </div>
+
+            <div class="next-button-container">
+                <button onclick="handleNextQuestion()">Submit</button>
+            </div>
+
+        </div>
+    </main>
+    <script src="Game.js"></script>
 </body>
-<script>
-
-</script>
-
-</html>
