e1f0be4c92bfad44bd694b64b00c09df9f82a35c
dailey2 <dailey2@kenyon.edu>
2022-04-14 20:22:55 -0400

    Update Home.php

--- Test/Attempt3/Home.php
+++ Test/Attempt3/Home.php
@@ -2,12 +2,12 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    
+
     <title>Login Sucessful
     </title>
 </head>
 <body>
-    <h1>Logged in!!!</h1>
+    <h1 style="text-align:center">Logged in!!!</h1>
     <h2>Press button to play </h2>
     <button type ="submit" onclick = "goToGame()">Play</button>
     <button type ="submit" onclick = "logOut()">Log out</button>
