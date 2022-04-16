c7c5e8e6e730608a865a84bdf9237fcb10448cd1
srabieh <rabieh1@kenyon.edu>
2022-04-16 17:27:05 -0400

    breaking it down — new file JS2PHPtester to make sure i'm trying to do the right thing with the minutiae

---
+++ Test/Attempt3/JS2PHPtester.php
@@ -0,0 +1,65 @@
+<?php
+
+
+
+function connect() {
+    $conn = mysqli_connect("localhost","fishell1","S219352","Game2");
+    
+    if (mysqli_connect_errno()) {
+        echo "Failed to connect to MySQL: " . mysqli_connect_error();
+        exit();
+    }
+    return $conn;
+}//Connection Function   
+    
+    
+    
+    
+function promptRequest(){
+    
+      $rowNumber = rand(1,300);
+    
+      
+        $prompt = "";
+          $conn = connect();
+          $sql = "SELECT * FROM QuestionDatabase WHERE QuestionNumber = '$rowNumber'";
+            if ($result = mysqli_query($conn, $sql)) {
+           
+                $row = mysqli_fetch_row($result);
+      
+        //      $prompt = $row[1];
+                
+              
+                mysqli_free_result($result);
+           
+                mysqli_close($conn);
+      
+                return json_encode($row);
+            }
+    }
+
+
+?>
+
+<script>
+    class Question {
+        qPrompt, qAns, qResponse1, qResponse2, qResponse3, qResponse4;
+        constructor ()
+        {
+            //how to parse JSON into these things...?  maybe call a new function for each?  (bring those back in code?)
+            var phpQuestion = '<?php echo promptRequest();?>';
+            var question = JSON.parse(phpQuestion);
+            this.qPrompt = phpQuestion[1];
+            this.qAns = phpQuestion[2];
+            this.qResponse1 = phpQuestion[2]
+            this.qResponse2 = phpQuestion[3];
+            this.qResponse3 = phpQuestion[4];
+            this.qResponse4 = phpQuestion[5];
+            console.log("${qPrompt} is qPrompt");
+        }
+    }
+
+    let q1 = new Question();
+    console.log(q1.qPrompt + " = q1.qPrompt1");
+
+</script>
