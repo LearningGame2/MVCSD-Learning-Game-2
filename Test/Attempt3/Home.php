0b311e18873e120f111eee54a0de484d788e6518
dailey2 <dailey2@kenyon.edu>
2022-04-17 17:17:42 -0400

    Update Home.php

--- Test/Attempt3/Home.php
+++ Test/Attempt3/Home.php
@@ -41,26 +41,25 @@
 </head>
 
 <body style="background-color:black;">
-  <div>
-    <h1 style="text-align:center; color:white;">
-      <span style="float:left; font-family:Copperplate">
-        MVCSD
-      </span>
-      <span style="float:right; font-family:Copperplate">
-        <a href="#">Home</a>
-        &nbsp;
-        <a href="#">Log In</a>
-        &nbsp;
-        <a href="#">About</a>
-        &nbsp;
-        <a href="#">Contact</a>
-        &nbsp;
-        <a href="#">Info</a>
-        &nbsp;
-      </span>
-    </h1>
-  </div>
 
+  <h1 style="text-align:center; color:white;">
+    <span style="float:left; font-family:Copperplate">
+      MVCSD
+    </span>
+    <span style="float:right; font-family:Copperplate">
+      <a href="#">Home</a>
+      &nbsp;
+      <a href="#">Log In</a>
+      &nbsp;
+      <a href="#">About</a>
+      &nbsp;
+      <a href="#">Contact</a>
+      &nbsp;
+      <a href="#">Info</a>
+      &nbsp;
+    </span>
+  </h1>
+  <br>
   <h2 style="text-align:center; color:white;">Press button to play </h2>
 
   <button type ="submit" onclick = "goToGame()" class="button button1">Play</button>
