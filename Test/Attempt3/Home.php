83db49aef7d1abd92c3d55a86d8bc208add33a80
dailey2 <dailey2@kenyon.edu>
2022-04-17 14:44:43 -0400

    button edit

--- Test/Attempt3/Home.php
+++ Test/Attempt3/Home.php
@@ -6,13 +6,30 @@
     <title>
         Login Sucessful
     </title>
+
+    <style>
+        .button {
+          background-color: #4CAF50;
+          border: none;
+          color: white;
+          padding: 15px 32px;
+          text-align: center;
+          text-decoration: none;
+          display: inline-block;
+          font-size: 16px;
+          margin: 4px 2px;
+          cursor: pointer;
+                }
+    </style>
 </head>
+
 <body>
     <h1 style="text-align:center;">Logged in!</h1>
     <h2 style="text-align:center;">Press button to play </h2>
-    <button type ="submit" onclick = "goToGame()" style="background-color:blue;">Play</button>
+    <button type ="submit" onclick = "goToGame()" class="button">Play</button>
     <button type ="submit" onclick = "logOut()">Log out</button>
 </body>
+
 <script>
 function goToGame(){
     window.location.href = "Game.php"
