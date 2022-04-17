e84211e0398bcca244721a7fe09abf9a0b4a4f8a
srabieh <rabieh1@kenyon.edu>
2022-04-17 16:37:55 -0400

    trying back to the original tutorial

--- AccountLogin/LoginAuth.php
+++ AccountLogin/LoginAuth.php
@@ -9,7 +9,7 @@ if($_SERVER["REQUEST_METHOD"] == "POST") {
    $myusername = mysqli_real_escape_string($db,$_POST['Username']);
    $mypassword = mysqli_real_escape_string($db,$_POST['Password']); 
    
-   $sql = "SELECT id FROM admin WHERE Username = '$myusername' and Password = '$mypassword'";
+   $sql = "SELECT StudentID FROM UserDatabase WHERE Username = '$myusername' and Password = '$mypassword'";
    $result = mysqli_query($db,$sql);
    $row = mysqli_fetch_array($result,MYSQLI_ASSOC);
    $active = $row['active'];
