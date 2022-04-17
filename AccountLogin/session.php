89ff257b4055f28942900e9083405ef6464f3ea2
srabieh <rabieh1@kenyon.edu>
2022-04-17 13:26:55 -0400

    i shouldn't have tried to compress the tutorial's files

--- AccountLogin/session.php
+++ AccountLogin/session.php
@@ -11,7 +11,7 @@
    $login_session = $row['username'];
    
    if(!isset($_SESSION['login_user'])){
-      header("location:login.php");
+      header("location:LoginAuth.php");
       die();
    }
 ?>
