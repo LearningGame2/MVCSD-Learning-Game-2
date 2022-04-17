e1f23d66096d0bb2627237cbfbbcf91c75f22efc
dailey2 <dailey2@kenyon.edu>
2022-04-17 15:57:32 -0400

    button adjust

--- Test/Attempt3/Home.php
+++ Test/Attempt3/Home.php
@@ -45,8 +45,8 @@
   <h1 style="text-align:center;">Logged in!</h1>
   <h2 style="text-align:center;">Press button to play </h2>
 
-  <button type ="submit" onclick = "goToGame()" class="button center">Play</button>
-  <button type ="submit" onclick = "logOut()" class="button center">Log out</button>
+  <button type ="submit" onclick = "goToGame()" class="button button1">Play</button>
+  <button type ="submit" onclick = "logOut()" class="button button2">Log out</button>
 </body>
 
 <script>
