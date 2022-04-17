7398c2598ff1eb3fe091e2710a36dd20a99e1f6e
srabieh <rabieh1@kenyon.edu>
2022-04-17 16:27:55 -0400

    trying to set up js within php, only using JSON.parse and js array for that tho

--- Test/Attempt3/Game.php
+++ Test/Attempt3/Game.php
@@ -30,10 +30,39 @@ function promptRequest(){
               mysqli_free_result($result);
            
            mysqli_close($conn);
+           echo "about to return ";
+           echo '$';
+           echo " row";
       
             return json_encode($row);
             }
     }
+
+    class Question {
+        public $prompt;
+        public cAns;
+        public wAns1;
+        public wAns2;
+        public wAns3;
+
+        function __construct() {
+            $phpQuestion = promptRequest();
+            echo "this is phpQuestion: ";
+            echo $phpQuestion;
+            $question = JSON.parse(phpQuestion);
+            echo "this is question: ";
+            echo $question;
+            $prompt = $question[1]; //this line might not work
+            echo "this is prompt: ";
+            echo $prompt;
+
+            // $prompt = $question[1];
+            // this.qPrompt = phpQuestion[1];
+            
+        }
+    }
+
+    //now put script for array of php objects...?  maybe you can echo for each thing of an object into a js object for array
 
 ?>
 
