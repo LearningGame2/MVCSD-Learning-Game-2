4cd3daacaf784ec22943d3741566abc868a6769b
dailey2 <dailey2@kenyon.edu>
2022-04-17 16:28:42 -0400

    Update Home.php

--- Test/Attempt3/Home.php
+++ Test/Attempt3/Home.php
@@ -49,9 +49,9 @@
 
 <body style="background-color:black;">
   <h1 style="text-align:center; color:white;">
-    <h1 style="float:left; font-family:Copperplate">
+    <span style="float:left; font-family:Copperplate">
       MVCSD
-    </h1>
+    </span>
     <span style="float:right; font-family:Copperplate">
       <a href="#">Home</a>
       &nbsp;
