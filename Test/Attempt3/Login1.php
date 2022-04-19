e4a5d356a7221465f35a0df398f24d31fa8d3a76
dailey2 <dailey2@kenyon.edu>
2022-04-19 16:13:14 -0400

    Update Login1.php

--- Test/Attempt3/Login1.php
+++ Test/Attempt3/Login1.php
@@ -14,15 +14,15 @@
 
 <body>
   <div class ="container" style = "position:relative;top:-10px;">
-    <h1 style="text-align:center; color:white; font-family:'Courier New', monospace; font-size:350%; position:relative; top:-10px;">
-      Welcome Back!
+    <h1 style="text-align:center; color:white; font-family:Garamond; font-size:350%; position:relative; top:-10px;">
+      Welcome Back to Learning Game 2!
     </h1>
-    <h4 style="text-align:center; color:white; font-family:'Courier New', monospace; font-size:150%; position:relative; top:-10px;">
+    <h4 style="text-align:center; color:white; font-family:Garamond; font-size:150%; position:relative; top:-10px;">
       To stay connected with us, please sign in.
     </h4>
     <form action = "" method = "post" class="login_form" autocomplete="off" name="form">
-      <label style="color:white;"> Username: </label><input type = "text" name = "username" class = "box"/><br /><br />
-      <label style="color:white;"> Password: </label><input type = "password" name = "password" class = "box" /><br/><br />
+      <label style="color:white; font-family:Garamond"> Username: </label><input type = "text" name = "username" class = "box"/><br /><br />
+      <label style="color:white; font-family:Garamond"> Password: </label><input type = "password" name = "password" class = "box" /><br/><br />
       <input type = "submit" value = " Submit " syle="text-align:center; color:blue; position:relative; left:50%"/><br />
     </form>
 
