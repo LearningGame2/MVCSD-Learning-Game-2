40f946fe4deaabc1b92bb0162ed5332541556aad
MasonFishell <fishell1@kenyon.edu>
2022-04-19 08:31:18 -0400

    jfdsa;

--- Test/Attempt3/Login1.php
+++ Test/Attempt3/Login1.php
@@ -1,122 +1,89 @@
 <?php
-function connect() {
-  $conn = mysqli_connect("localhost","fishell1","S219352","Game2");
-  
-  if (mysqli_connect_errno()) {
-      echo "Failed to connect to MySQL: " . mysqli_connect_error();
-      exit();
-  }
-  return $conn;
-}//Connection Function   
-
-
-
-function userLogInRequest(){
-  
-$numOfUsers = mysqli_num_rows();
-  $conn = connect();
-  $sql = "SELECT * FROM UserDatabase";
-  
-  mysqli_close($conn);
-  //return json_encode($row);
-
-  $penis = array("sex", "sexagain");
-  $User = array();
-  for ($x = 0; $x < $numOfUsers; $x++) {
-    if ($result = mysqli_query($conn, $sql)) {
-      $User[$x] = json_encod(mysqli_fetch_row($result));
-      mysqli_free_result($result);
-  }
-  return $numOfUsers;
-  }
-
+   function connect() {
+    $conn = mysqli_connect("localhost","fishell1","S219352","Game2");
+    
+    if (mysqli_connect_errno()) {
+        echo "Failed to connect to MySQL: " . mysqli_connect_error();
+        exit();
+    }
+    return $conn;
 }
+  $conn = connect();
+   
+   if($_SERVER["REQUEST_METHOD"] == "POST") {
+      // username and password sent from form 
+      
+      $myusername = mysqli_real_escape_string($conn,$_POST['username']);
+      $mypassword = mysqli_real_escape_string($conn,$_POST['password']); 
+      
+      $sql = "SELECT StudentID FROM admin WHERE Username = '$myusername' and Passcode = '$mypassword'";
+      $result = mysqli_query($db,$sql);
+      $row = mysqli_fetch_array($result,MYSQLI_ASSOC);
+      $active = $row['active'];
+      
+      $count = mysqli_num_rows($result);
+      
+      // If result matched $myusername and $mypassword, table row must be 1 row
+		
+      if($count == 1) {
+         session_register("myusername");
+         $_SESSION['login_user'] = $myusername;
+         
+         header("location: Home.php");
+      }else {
+         $error = "Your Login Name or Password is invalid";
+      }
+   }
 ?>
-
 
 
 
 
 <!DOCTYPE html>
 <html lang="en">
+   
+   <head>
+      <title>Login Page</title>
+      
+      <style type = "text/css">
+         body {
+            font-family:Arial, Helvetica, sans-serif;
+            font-size:14px;
+         }
+         label {
+            font-weight:bold;
+            width:100px;
+            font-size:14px;
+         }
+         .box {
+            border:#666666 solid 1px;
+         }
+      </style>
+      
+   </head>
+   
+   <body bgcolor = "#FFFFFF">
+	
+      <div align = "center">
+         <div style = "width:300px; border: solid 1px #333333; " align = "left">
+            <div style = "background-color:#333333; color:#FFFFFF; padding:3px;"><b>Login</b></div>
+				
+            <div style = "margin:30px">
+               
+               <form action = "" method = "post">
+                  <label>UserName  :</label><input type = "text" name = "username" class = "box"/><br /><br />
+                  <label>Password  :</label><input type = "password" name = "password" class = "box" /><br/><br />
+                  <input type = "submit" value = " Submit "/><br />
+               </form>
+               
+               <div style = "font-size:11px; color:#cc0000; margin-top:10px"><?php echo $error; ?></div>
+					
+            </div>
+				
+         </div>
+			
+      </div>
 
-<head>
-    <meta charset = "UTF-8">
-    <title> Validated Login Form</title>
-    <link rel="stylesheet" href="LoginStyle.css">
-</head>
-
-<body>
-  <div class ="container" style = "position:relative;top:-10px;">
-    <h1 style="text-align:center; color:white; font-family:Fantasy; font-size:350%; position:relative; top:-10px;">
-      Welcome Back!
-    </h1>
-    <h4 style="text-align:center; color:white; font-family:Fantasy; font-size:150%; position:relative; top:-10px;">
-      To stay connected with us, please sign in.
-    </h4>
-    <form class="login_form" autocomplete="off" action = "" method="post" name="form" onsubmit= "validated(); return false;">
-        <div class ="font" style="font-family:Fantasy; font-size:125%;">Username</div>
-        <input type="text" name = "email">
-        <div id = "email_error">Incorrect</div>
-        <div class ="font font2" style="font-family:Fantasy; font-size:125%;">Password</div>
-        <input type="password" name = "password">
-        <div id = "pass_error">Incorrect</div>
-        <button type = "submit">Sign In</button>
-    </form>
-  </div>
-</body>
-
-<script>
-
-var UsersData = '<?php echo userLogInRequest();?>'
-console.log(UsersData);
-
-var email = document.forms['form']['email'];
-var password = document.forms['form']['password'];
-
-var email_error = document.getElementById('email_error');
-var pass_error = document.getElementById('pass_error');
-
-email.addEventListener('textInput', email_verify);
-password.addEventListener('textInput', pass_verify); 
-
-function validated(){
-    if(email.value.length >8 & password.value.length >5){
-        window.location.href = "Home.php";
-        return false;
-    }
-    if(email.value.length < 9){
-        email.style.border = "1px solid red";
-        email_error.style.display= "block";
-        email.focus();
-        return false;
-    }
-    if(password.value.length < 6){
-        password.style.border = "1px solid red";
-        pass_error.style.display= "block";
-        password.focus(); 
-        return false;
-    }
-
-
-}
-function email_verify(){
-    if(email.value.length >=8){
-        email.style.border = "1px solid silver";
-        email_error.style.display= "none";
-        return true;
-        
-    }
-}
-function pass_verify(){
-    if(password.value.length >5){
-        password.style.border = "1px solid silver";
-        pass_error.style.display= "none";
-        return true;
-    }
-}
-
-
-</script>
+   </body>
 
 </html>
