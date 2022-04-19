d7b87f6f995f014b32c641223d1be387731d54e3
dailey2 <dailey2@kenyon.edu>
2022-04-19 16:43:34 -0400

    revert

--- Test/Attempt3/Home.php
+++ Test/Attempt3/Home.php
@@ -65,7 +65,7 @@
 <header class="page-header">
   &nbsp;&nbsp;&nbsp;&nbsp;
   <h1 style="text-align:left; color:white;">
-    Welcome <?php echo $user; ?>!
+    Welcome <?php echo $myusername; ?>!
   </h1>
 </header>
 
