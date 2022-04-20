7dab29a5033370f2300c22da0c2ef68cc97a55ba
dailey2 <dailey2@kenyon.edu>
2022-04-19 20:57:38 -0400

    Update Login1.php

--- Test/Attempt3/Login1.php
+++ Test/Attempt3/Login1.php
@@ -41,7 +41,7 @@
 
 <body>
   <div class ="container" style = "position:relative;top:-10px;">
-    <h1 style="text-align:center; color:white; font-family:Garamond; font-size:350%; position:relative; top:-10px;">
+    <h1 style="text-align:center; color:blue; font-family:Garamond; font-size:350%; position:relative; top:-10px;">
       Welcome Back!
     </h1>
     <h4 style="text-align:center; color:white; font-family:Garamond; font-size:150%; position:relative; top:-10px;">
@@ -53,7 +53,7 @@
       <button type = "submit" value = " Submit ">Submit</button>
     </form>
 
-    <div style = "font-size:11px; color:blue; margin-top:10px; text-align:center; position:relative; left:10%">
+    <div style = "font-size:11px; color:red; margin-top:10px; text-align:center; position:relative; left:10%">
       <?php echo $error; ?>
     </div>
   </div>
