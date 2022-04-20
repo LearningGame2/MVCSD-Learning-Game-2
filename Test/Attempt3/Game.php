d391151ba1417ec61f56a2db48dc674c195244df
srabieh <rabieh1@kenyon.edu>
2022-04-19 21:23:48 -0400

    changing Game.php to working code — trying to de facto do a merge before actually merging cuz these are messed up

--- Test/Attempt3/Game.php
+++ Test/Attempt3/Game.php
@@ -1,75 +1,442 @@
-<!--<html>
-
-
 <?php
-
-print "testPrint";
-
-
 function connect() {
     $conn = mysqli_connect("localhost","fishell1","S219352","Game2");
-    
+
     if (mysqli_connect_errno()) {
         echo "Failed to connect to MySQL: " . mysqli_connect_error();
         exit();
     }
     return $conn;
-}//Connection Function   
-    
-    
-    
-    
-function promptRequest(){
-    
-      $rowNumber = rand(1,300);
-    
-      
+}//Connection Function
+
+
+
+
+function promptRequest($numOfQuestions){
+    $questions = array();
+    for ($x = 0; $x <= $numOfQuestions; $x++) {
+        $rowNumber = rand(1,300);
         $prompt = "";
-          $conn = connect();
-          $sql = "SELECT * FROM QuestionDatabase WHERE QuestionNumber = '$rowNumber'";
-          if ($result = mysqli_query($conn, $sql)) {
-           
-              $row = mysqli_fetch_row($result);
-      
-        //      $prompt = $row[1];
-                
-              
-              mysqli_free_result($result);
-           
-           mysqli_close($conn);
-           echo "about to return " + "$" + " row";
-      
-            return json_encode($row);
-            }
+        $conn = connect();
+        $sql = "SELECT * FROM QuestionDatabase WHERE QuestionNumber = '$rowNumber'";
+        if ($result = mysqli_query($conn, $sql)) {
+
+            $row = mysqli_fetch_row($result);
+
+      //      $prompt = $row[1];
+
+
+            mysqli_free_result($result);
+
+         mysqli_close($conn);
+
+          $questions[$x] = $row;
+      }
     }
+  return json_encode($questions);
+}
 
-    class Question {
-        public $prompt;
-        public cAns;
-        public wAns1;
-        public wAns2;
-        public wAns3;
 
-        function __construct() {
-            $phpQuestion = promptRequest();
-            echo "this is phpQuestion: ";
-            echo $phpQuestion;
-            $question = JSON.parse(phpQuestion);
-            echo "this is question: ";
-            echo $question;
-            $prompt = $question[1]; //this line might not work
-            echo "this is prompt: ";
-            echo $prompt;
-
-            // $prompt = $question[1];
-            // this.qPrompt = phpQuestion[1];
-            
-        }
-    }
-
-    //now put script for array of php objects...?  maybe you can echo for each thing of an object into a js object for array
 
 ?>
 
 
-</html>
+
+<head>
+  <link rel="stylesheet" href="Game.css">
+</head>
+
+<body onload="NextQuestion(0)">
+  <h1 style="background-color:black; color:white; text-align:center; font-family:courier new; font-size:300%; line-height: 100px">
+    Learning Game 2
+  </h1>
+    <main>
+        <!-- creating a modal for when quiz ends -->
+        <div class="modal-container" id="score-modal">
+
+            <div class="modal-content-container">
+
+                <h1>Congratulations, Quiz Completed.</h1>
+
+                <div class="grade-details">
+                    <p>Attempts : 10</p>
+                    <p>Wrong Answers : <span id="wrong-answers"></span></p>
+                    <p>Right Answers : <span id="right-answers"></span></p>
+                    <p>Grade : <span id="grade-percentage"></span>%</p>
+                    <p ><span id="remarks"></span></p>
+                </div>
+
+                <div class="modal-button-container">
+                    <button onclick="closeScoreModal()">Play Again</button>
+                    <button onclick="returnHome()">Exit Game</button>
+                </div>
+
+            </div>
+        </div>
+<!-- end of modal of quiz details-->
+
+        <div class="game-quiz-container" style = "position:relative; top:-75px">
+
+            <div class="game-details-container">
+                <h1>Score : <span id="player-score"></span> / 10</h1>
+                <h1> Question : <span id="question-number"></span> / 10</h1>
+            </div>
+
+            <div class="game-question-container">
+                <h1 id="display-question"></h1>
+            </div>
+
+            <div class="game-options-container">
+
+               <div class="modal-container" id="option-modal">
+
+                    <div class="modal-content-container">
+                         <h1>Please Pick An Option</h1>
+
+                         <div class="modal-button-container">
+                            <button onclick="closeOptionModal()">Continue</button>
+                        </div>
+
+                    </div>
+
+               </div>
+
+                <span>
+                    <input type="radio" id="option-one" name="option" class="radio" value="optionA" />
+                    <label for="option-one" class="option" id="option-one-label"></label>
+                </span>
+
+
+                <span>
+                    <input type="radio" id="option-two" name="option" class="radio" value="optionB" />
+                    <label for="option-two" class="option" id="option-two-label"></label>
+                </span>
+
+
+                <span>
+                    <input type="radio" id="option-three" name="option" class="radio" value="optionC" />
+                    <label for="option-three" class="option" id="option-three-label"></label>
+                </span>
+
+
+                <span>
+                    <input type="radio" id="option-four" name="option" class="radio" value="optionD" />
+                    <label for="option-four" class="option" id="option-four-label"></label>
+                </span>
+
+            </div>
+
+            <div class="next-button-container">
+                <button onclick="handleNextQuestion()">Submit</button>
+            </div>
+
+        </div>
+    </main>
+    <script>
+
+
+
+
+
+
+function fillQuestions(){
+    const questions = [
+        //Question 1
+            {
+                question: "",
+                optionA: "",
+                optionB: "",
+                optionC: "",
+                optionD: "",
+                correctOption: ""
+            },
+        //Question 2
+        {
+            question: "",
+            optionA: "",
+            optionB: "",
+            optionC: "",
+            optionD: "",
+            correctOption: ""
+        },
+        //Question 3
+        {
+            question: "",
+            optionA: "",
+            optionB: "",
+            optionC: "",
+            optionD: "",
+            correctOption: ""
+        },
+        //Question 4
+        {
+            question: "",
+            optionA: "",
+            optionB: "",
+            optionC: "",
+            optionD: "",
+            correctOption: ""
+        },
+        //Question 5
+        {
+            question: "",
+            optionA: "",
+            optionB: "",
+            optionC: "",
+            optionD: "",
+            correctOption: ""
+        },
+        //Question 6
+        {
+            question: "",
+            optionA: "",
+            optionB: "",
+            optionC: "",
+            optionD: "",
+            correctOption: ""
+        },
+        //Question 7
+        {
+            question: "",
+            optionA: "",
+            optionB: "",
+            optionC: "",
+            optionD: "",
+            correctOption: ""
+        },
+        //Question 8
+        {
+            question: "",
+            optionA: "",
+            optionB: "",
+            optionC: "",
+            optionD: "",
+            correctOption: ""
+        },
+        //Question 9
+        {
+            question: "",
+            optionA: "",
+            optionB: "",
+            optionC: "",
+            optionD: "",
+            correctOption: ""
+        },
+        //Question 10
+        {
+            question: "",
+            optionA: "",
+            optionB: "",
+            optionC: "",
+            optionD: "",
+            correctOption: ""
+        }
+];
+
+    var phpPrompt = JSON.parse('<?php echo promptRequest(10);?>');
+  for (let i = 0; i < 10; i++) {
+    var prompt = phpPrompt[i];
+    console.log(prompt);
+    questions[i].question = prompt[1];
+
+    var j = Math.floor(Math.random()*4)+1;
+
+    if(j == 1){
+        questions[i].optionA = prompt[2];
+        questions[i].optionB = prompt[3];
+        questions[i].optionC = prompt[4];
+        questions[i].optionD = prompt[5];
+        questions[i].correctOption ="optionA";
+    }
+    if(j == 2){
+        questions[i].optionA = prompt[5];
+        questions[i].optionB = prompt[2];
+        questions[i].optionC = prompt[3];
+        questions[i].optionD = prompt[4];
+        questions[i].correctOption = "optionB";
+    }
+    if(j == 3){
+        questions[i].optionA = prompt[4];
+        questions[i].optionB = prompt[5];
+        questions[i].optionC = prompt[2];
+        questions[i].optionD = prompt[3];
+        questions[i].correctOption = "optionC";
+    }
+    if(j == 4){
+        questions[i].optionA = prompt[3];
+        questions[i].optionB = prompt[4];
+        questions[i].optionC = prompt[5];
+        questions[i].optionD = prompt[2];
+        questions[i].correctOption = "optionD";
+    }
+}
+    return questions;
+}
+
+
+let shuffledQuestions = []; //empty array to hold shuffled selected questions out of all available questions
+
+function handleQuestions() {
+    questions = fillQuestions();
+        //function to shuffle and push 10 questions to shuffledQuestions array
+    //app would be dealing with 10questions per session
+        while (shuffledQuestions.length <= 9) {
+            const random = questions[Math.floor(Math.random() * questions.length)]
+            if (!shuffledQuestions.includes(random)) {
+                shuffledQuestions.push(random)
+            }
+        }
+}
+
+
+    let questionNumber = 1 //holds the current question number
+    let playerScore = 0  //holds the player score
+    let wrongAttempt = 0 //amount of wrong answers picked by player
+    let indexNumber = 0 //will be used in displaying next question
+
+    // function for displaying next question in the array to dom
+    //also handles displaying players and quiz information to dom
+    function NextQuestion(index) {
+        handleQuestions()
+        const currentQuestion = shuffledQuestions[index]
+        document.getElementById("question-number").innerHTML = questionNumber
+        document.getElementById("player-score").innerHTML = playerScore
+        document.getElementById("display-question").innerHTML = currentQuestion.question;
+        document.getElementById("option-one-label").innerHTML = currentQuestion.optionA;
+        document.getElementById("option-two-label").innerHTML = currentQuestion.optionB;
+        document.getElementById("option-three-label").innerHTML = currentQuestion.optionC;
+        document.getElementById("option-four-label").innerHTML = currentQuestion.optionD;
+
+    }
+
+
+    function checkForAnswer() {
+        const currentQuestion = shuffledQuestions[indexNumber] //gets current Question
+        const currentQuestionAnswer = currentQuestion.correctOption //gets current Question's answer
+        const options = document.getElementsByName("option"); //gets all elements in dom with name of 'option' (in this the radio inputs)
+        let correctOption = null
+
+        options.forEach((option) => {
+            if (option.value === currentQuestionAnswer) {
+                //get's correct's radio input with correct answer
+                correctOption = option.labels[0].id
+            }
+        })
+
+        //checking to make sure a radio input has been checked or an option being chosen
+        if (options[0].checked === false && options[1].checked === false && options[2].checked === false && options[3].checked == false) {
+            document.getElementById('option-modal').style.display = "flex"
+        }
+
+        //checking if checked radio button is same as answer
+        options.forEach((option) => {
+            if (option.checked === true && option.value === currentQuestionAnswer) {
+                document.getElementById(correctOption).style.backgroundColor = "green"
+                playerScore++ //adding to player's score
+                indexNumber++ //adding 1 to index so has to display next question..
+                //set to delay question number till when next question loads
+                setTimeout(() => {
+                    questionNumber++
+                }, 1000)
+            }
+
+            else if (option.checked && option.value !== currentQuestionAnswer) {
+                const wrongLabelId = option.labels[0].id
+                document.getElementById(wrongLabelId).style.backgroundColor = "red"
+                document.getElementById(correctOption).style.backgroundColor = "green"
+                wrongAttempt++ //adds 1 to wrong attempts
+                indexNumber++
+                //set to delay question number till when next question loads
+                setTimeout(() => {
+                    questionNumber++
+                }, 1000)
+            }
+        })
+    }
+
+
+
+    //called when the next button is called
+    function handleNextQuestion() {
+        checkForAnswer() //check if player picked right or wrong option
+        unCheckRadioButtons()
+        //delays next question displaying for a second just for some effects so questions don't rush in on player
+        setTimeout(() => {
+            if (indexNumber <= 9) {
+    //displays next question as long as index number isn't greater than 9, remember index number starts from 0, so index 9 is question 10
+                NextQuestion(indexNumber)
+            }
+            else {
+                handleEndGame()//ends game if index number greater than 9 meaning we're already at the 10th question
+            }
+            resetOptionBackground()
+        }, 1000);
+    }
+
+    //sets options background back to null after display the right/wrong colors
+    function resetOptionBackground() {
+        const options = document.getElementsByName("option");
+        options.forEach((option) => {
+            document.getElementById(option.labels[0].id).style.backgroundColor = ""
+        })
+    }
+
+    // unchecking all radio buttons for next question(can be done with map or foreach loop also)
+    function unCheckRadioButtons() {
+        const options = document.getElementsByName("option");
+        for (let i = 0; i < options.length; i++) {
+            options[i].checked = false;
+        }
+    }
+
+    // function for when all questions being answered
+    function handleEndGame() {
+        let remark = null
+        let remarkColor = null
+
+        // condition check for player remark and remark color
+        if (playerScore <= 3) {
+            remark = "You can do better!"
+            remarkColor = "red"
+        }
+        else if (playerScore >= 4 && playerScore < 7) {
+            remark = "Keep practicing!"
+            remarkColor = "orange"
+        }
+        else if (playerScore >= 7) {
+            remark = "Excellent! Keep up the good work."
+            remarkColor = "green"
+        }
+        const playerGrade = (playerScore / 10) * 100
+
+        //data to display to score board
+        document.getElementById('remarks').innerHTML = remark
+        document.getElementById('remarks').style.color = remarkColor
+        document.getElementById('grade-percentage').innerHTML = playerGrade
+        document.getElementById('wrong-answers').innerHTML = wrongAttempt
+        document.getElementById('right-answers').innerHTML = playerScore
+        document.getElementById('score-modal').style.display = "flex"
+
+    }
+
+    //closes score modal, resets game and reshuffles questions
+    function closeScoreModal() {
+        questionNumber = 1
+        playerScore = 0
+        wrongAttempt = 0
+        indexNumber = 0
+        shuffledQuestions = []
+        NextQuestion(indexNumber)
+        document.getElementById('score-modal').style.display = "none"
+    }
+
+    //function to close warning modal
+    function closeOptionModal() {
+        document.getElementById('option-modal').style.display = "none"
+    }
+
+    function returnHome(){
+        window.location.href = "Home.php"
+    }
+    </script>
+</body>
