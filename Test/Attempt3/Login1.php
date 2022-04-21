abf6e3a504d497f6a29161be3cbd9df663ade5d5
srabieh <rabieh1@kenyon.edu>
2022-04-20 22:39:20 -0400

    if anyone actually ever reads this stuff...you know the drill by now

--- Test/Attempt3/Login1.php
+++ Test/Attempt3/Login1.php
@@ -2,82 +2,61 @@
 
   $conn = mysqli_connect("localhost","fishell1","S219352","Game2");
   session_start();
-   
+
    if($_SERVER["REQUEST_METHOD"] == "POST") {
-      // username and password sent from form 
-      
+      // username and password sent from form
+
       $myusername = mysqli_real_escape_string($conn,$_POST['username']);
-      $mypassword = mysqli_real_escape_string($conn,$_POST['password']); 
-      
+      $mypassword = mysqli_real_escape_string($conn,$_POST['password']);
+
       $sql = "SELECT StudentID FROM UserDatabase WHERE Username = '$myusername' and Passcode = '$mypassword'";
       $result = mysqli_query($conn,$sql);
       $row = mysqli_fetch_array($result,MYSQLI_ASSOC);
       $active = $row['active'];
-      
+
       $count = mysqli_num_rows($result);
-      
+
       // If result matched $myusername and $mypassword, table row must be 1 row
-		
+
       if($count == 1) {
          //session_register("myusername");
-         //$_SESSION['login_user'] = $myusername;
+         $_SESSION['login'] = $myusername;
          $error = "Login Sucessful";
-         header("location: http://cslab.kenyon.edu/class/ssd/Game2/LGAttempt3/Game.php");
+         header("location: http://cslab.kenyon.edu/class/ssd/Game2/LGAttempt3/Home.php");
       }else {
-         $error = "Your Login Name or Password is invalid";
- 
+         $error = "Your login name or password is invalid.";
+
       }
    }
 ?>
 
-
-
-
 <!DOCTYPE html>
 <html lang="en">
-   
-   <head>
-      <title>Login Page</title>
-      
-      <style type = "text/css">
-         body {
-            font-family:Arial, Helvetica, sans-serif;
-            font-size:14px;
-         }
-         label {
-            font-weight:bold;
-            width:100px;
-            font-size:14px;
-         }
-         .box {
-            border:#666666 solid 1px;
-         }
-      </style>
-      
-   </head>
-   
-   <body bgcolor = "#FFFFFF">
-	
-      <div align = "center">
-         <div style = "width:300px; border: solid 1px #333333; " align = "left">
-            <div style = "background-color:#333333; color:#FFFFFF; padding:3px;"><b>Login</b></div>
-				
-            <div style = "margin:30px">
-               
-               <form action = "" method = "post">
-                  <label>UserName  :</label><input type = "text" name = "username" class = "box"/><br /><br />
-                  <label>Password  :</label><input type = "password" name = "password" class = "box" /><br/><br />
-                  <input type = "submit" value = " Submit "/><br />
-               </form>
-               
-               <div style = "font-size:11px; color:#cc0000; margin-top:10px"><?php echo $error; ?></div>
-					
-            </div>
-				
-         </div>
-			
-      </div>
 
-   </body>
+<head>
+  <meta charset = "UTF-8">
+  <title>Login Form</title>
+  <link rel="stylesheet" href="LoginStyle.css">
+</head>
+
+<body>
+  <div class ="container" style = "position:relative; top:-10px;">
+    <h1 style="text-align:center; color:white; font-family:Garamond; font-size:350%; position:relative; top:-10px;">
+      Welcome Back!
+    </h1>
+    <h4 style="text-align:center; color:white; font-family:Garamond; font-size:150%; position:relative; top:-10px;">
+      To stay connected with us, please sign in.
+    </h4>
+    <form action = "" method = "post" class="login_form" autocomplete="off" name="form">
+      <label style="color:white; font-family:Garamond"> Username: </label><input type = "text" name = "username" class = "box"/><br /><br />
+      <label style="color:white; font-family:Garamond"> Password: </label><input type = "password" name = "password" class = "box" /><br/><br />
+      <button type = "submit" value = " Submit ">Submit</button>
+    </form>
+
+    <div style = "font-size:30px; color:red; margin-top:10px; text-align:center; position:relative; left:0%">
+      <?php echo $error; ?>
+    </div>
+  </div>
+</body>
 
 </html>
