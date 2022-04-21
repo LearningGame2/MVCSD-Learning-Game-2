cf2a472c007a781ae69814e7d961021ac75450f4
dailey2 <98828394+dailey2@users.noreply.github.com>
2022-04-21 10:27:36 -0400

    Update Home.php

--- Test/Attempt3/Home.php
+++ Test/Attempt3/Home.php
@@ -1,6 +1,12 @@
 
 <?php
-include 'UserInfo.php'
+
+session_start();
+
+// if(!isset($_SESSION['login'])){
+//   header("location: http://cslab.kenyon.edu/class/ssd/Game2/LGAttempt3/Login1.php");
+// } //Comment out to make debugging less annoying
+
 
 function connect() {
   $conn = mysqli_connect("localhost","fishell1","S219352","Game2");
@@ -14,28 +20,19 @@ function connect() {
 
 
 
-
-function highscoreRequest(){
+function leaderboardRequest(){
   $scores = array();
+
+  $conn = connect();
+  $sql = "SELECT * FROM Leaderboard";
+  $result = mysqli_query($conn,$sql);
   for ($x = 0; $x < 10; $x++) {
-      $conn = connect();
-      $sql = "SELECT * FROM Leaderboard";
-      if ($result = mysqli_query($conn, $sql)) {
-
-          $row = mysqli_fetch_row($result);
-
-    //      $prompt = $row[1];
-
-
-          mysqli_free_result($result);
-
-       mysqli_close($conn);
-
-        $scores[$x] = $row;
-    }
+    $scores[$x] = mysqli_fetch_assoc($result);
   }
-
-return json_encode($scores);
+  usort($scores, function($a, $b) {
+    return $a['Highscore'] <=> $b['Highscore'];
+  });
+  return json_encode($scores);
 }
 
 
@@ -71,7 +68,7 @@ return json_encode($scores);
     .button1 {
       margin: 0;
       position: absolute;
-      top: 50%;
+      top: 30%;
       left: 40%;
       -ms-transform: translate(-50%, -50%);
       transform: translate(-50%, -50%);
@@ -79,7 +76,7 @@ return json_encode($scores);
     .button2 {
       margin: 0;
       position: absolute;
-      top: 50%;
+      top: 30%;
       left: 60%;
       -ms-transform: translate(-50%, -50%);
       transform: translate(-50%, -50%);
@@ -98,40 +95,134 @@ return json_encode($scores);
     color: black;
     }
     table, th, td {
-      border: 1px solid black;
+      border: 2px solid white;
       border-collapse: collapse;
-      width:100%;
-      bordercolor:red;
+      margin-top:auto;
+      margin-left:auto;
+      width:40%;
+      text-align:center;
+      font-size:23px;
+      font-family: "Lucida Console", "Courier New", monospace;
     }
-    th, td {
-      padding: 15px;
+    .center {
+      margin-left: auto;
+      margin-right: auto;
+      margin-top:200px
     }
   </style>
 </head>
 
 <header class="page-header">
   &nbsp;&nbsp;&nbsp;&nbsp;
-  <h1 style="text-align:left; color:white;">
-    Welcome <?php echo $user ?> !
+  <h1 style="text-align:center; color:white;">
+    Welcome <?php echo $_SESSION['login']?>!
   </h1>
 </header>
 
 <body style="background-color:black;">
-  <div style = "text-align:center; position:relative; top:150px; font-size:48px; color:white;">
+  <div style = "text-align:center; position:relative; top:30px; font-size:48px; color:white;">
      Press Button to Play
   </div>
 
   <button type ="submit" onclick = "goToGame()" class="button button1">Play</button>
   <button type ="submit" onclick = "logOut()" class="button button2">Log out</button>
+
+  <div style = "text-align:center; position:relative; top:185px; font-size:48px; color:white;">
+      Leaderboard
+  </div>
+
+  <div>
+  <table style = "color:white;" class="center">
+  <tr>
+    <th>Player</th>
+    <th>Score</th>
+  </tr>
+  <tr>
+    <td><span id="player1"></span></td>
+    <td><span id="score1"></span></td>
+  </tr>
+  <tr>
+    <td><span id="player2"></span></td>
+    <td><span id="score2"></span></td>
+  </tr>
+  <tr>
+    <td><span id="player3"></span></td>
+    <td><span id="score3"></span></td>
+  </tr>
+  <tr>
+    <td><span id="player4"></span></td>
+    <td><span id="score4"></span></td>
+  </tr>
+  <tr>
+    <td><span id="player5"></span></td>
+    <td><span id="score5"></span></td>
+  </tr>
+  <tr>
+    <td><span id="player6"></span></td>
+    <td><span id="score6"></span></td>
+  </tr>
+  <tr>
+    <td><span id="player7"></span></td>
+    <td><span id="score7"></span></td>
+  </tr>
+  <tr>
+    <td><span id="player8"></span></td>
+    <td><span id="score8"></span></td>
+  </tr>
+  <tr>
+    <td><span id="player9"></span></td>
+    <td><span id="score9"></span></td>
+  </tr>
+  <tr>
+    <td><span id="player10"></span></td>
+    <td><span id="score10"></span></td>
+  </tr>
+</table>
+</div>
 </body>
 
 <script>
+  var testScores = JSON.parse('<?php echo leaderboardRequest();?>');
+
+  document.getElementById("player1").innerHTML = testScores[9].Username;
+  document.getElementById("score1").innerHTML = testScores[9].Highscore;
+
+  document.getElementById("player2").innerHTML = testScores[8].Username;
+  document.getElementById("score2").innerHTML = testScores[8].Highscore;
+
+  document.getElementById("player3").innerHTML = testScores[7].Username;
+  document.getElementById("score3").innerHTML = testScores[7].Highscore;
+
+  document.getElementById("player4").innerHTML = testScores[6].Username;
+  document.getElementById("score4").innerHTML = testScores[6].Highscore;
+
+  document.getElementById("player5").innerHTML = testScores[5].Username;
+  document.getElementById("score5").innerHTML = testScores[5].Highscore;
+
+  document.getElementById("player6").innerHTML = testScores[4].Username;
+  document.getElementById("score6").innerHTML = testScores[4].Highscore;
+
+  document.getElementById("player7").innerHTML = testScores[3].Username;
+  document.getElementById("score7").innerHTML = testScores[3].Highscore;
+
+  document.getElementById("player8").innerHTML = testScores[2].Username;
+  document.getElementById("score8").innerHTML = testScores[2].Highscore;
+
+  document.getElementById("player9").innerHTML = testScores[1].Username;
+  document.getElementById("score9").innerHTML = testScores[1].Highscore;
+
+  document.getElementById("player10").innerHTML = testScores[0].Username;
+  document.getElementById("score10").innerHTML = testScores[0].Highscore;
+
+  console.log(testScores);
   function goToGame(){
-    window.location.href = "Game.php"
+    window.location.href = "LevelOne.php"
   }
   function logOut(){
     window.location.href = "Login1.php"
+    <?php session_destroy(); ?>
   }
+
 </script>
 
 </html>
