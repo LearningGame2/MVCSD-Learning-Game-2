ee9d62769c25bfb856199a429737cb23d074a66e
srabieh <85193282+srabieh@users.noreply.github.com>
2022-04-20 21:29:31 -0400

    Deleting AccountLogin folder
    only made this for myself so i don't think anyone will mind

--- AccountLogin/config.php
+++
@@ -1,7 +0,0 @@
-<?php
-   define('DB_SERVER', 'localhost');
-   define('DB_USERNAME', 'fishell1');
-   define('DB_PASSWORD', 'S219352');
-   define('DB_DATABASE', 'Game2');
-   $db = mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
-?>
