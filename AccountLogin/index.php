c4889d04d321774e09de5ba5e74e2d385da36944
srabieh <rabieh1@kenyon.edu>
2022-04-20 22:36:44 -0400

    deleting files from my branch that we deleted on main, nothing to see here — Sam

--- AccountLogin/index.php
+++
@@ -1,7 +0,0 @@
-<!DOCTYPE html>
-<html lang = "en">
-    <head>
-        <link rel = "stylesheet" type = "text/css" href = "style.css">
-        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-    </head>
-</html>
