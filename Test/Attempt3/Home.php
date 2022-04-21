56f7c33aac8ca3ad3d60f6fc46b254e0ae6c1a61
srabieh <rabieh1@kenyon.edu>
2022-04-20 23:28:56 -0400

    renaming n shit

--- Test/Attempt3/Home.php
+++ Test/Attempt3/Home.php
@@ -216,7 +216,7 @@ function leaderboardRequest(){
 
   console.log(testScores);
   function goToGame(){
-    window.location.href = "Game.php" //CHANGE THIS IF CHANGE FOR EACH LEVEL
+    window.location.href = "LevelOne.php"
   }
   function logOut(){
     window.location.href = "Login1.php"
