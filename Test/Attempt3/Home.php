303b2ffa339bcd2d2d08ad2e36ae04611a90bec7
MasonFishell <fishell1@kenyon.edu>
2022-04-20 22:15:58 -0400

    hfdsa;

--- Test/Attempt3/Home.php
+++ Test/Attempt3/Home.php
@@ -3,9 +3,9 @@
 
 session_start();
 
-if(!isset($_SESSION['login'])){
-  header("location: http://cslab.kenyon.edu/class/ssd/Game2/LGAttempt3/Login1.php");
-} //Comment out to make less annoying
+// if(!isset($_SESSION['login'])){
+//   header("location: http://cslab.kenyon.edu/class/ssd/Game2/LGAttempt3/Login1.php");
+// } //Comment out to make less annoying
 
 
 function connect() {
