28631b71199652606681a8d5372b5fc2f541ea62
MasonFishell <fishell1@kenyon.edu>
2022-04-21 09:24:06 -0400

    fdsa

--- Test/Attempt3/Level2.php
+++ Test/Attempt3/Level2.php
@@ -35,10 +35,6 @@ function promptRequest($numOfQuestions){
   return json_encode($questions);
 }
 
-function addToScore($s){
-    $_SESSION['score'] = $_SESSION['score'] + $s;
-
-}
 
 ?>
 
@@ -209,7 +205,7 @@ let questionArray = fillQuestions();
     console.log('<?php echo $_SESSION['score'];?>')
 
     let questionNumber = 1 //holds the current question number
-    let playerScore = 0  //holds the player score
+    let playerScore = parseInt('<?php echo $_SESSION['score'];?>')  //holds the player score
     let amountCorrect = 0 //different from score, does not include streaks
     let wrongAttempt = 0 //amount of wrong answers picked by player
     let indexNumber = 0 //will be used in displaying next question
