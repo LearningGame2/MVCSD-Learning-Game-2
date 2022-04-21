a8b9629cce3cf777a27892094102fa3c18aa7a36
MasonFishell <fishell1@kenyon.edu>
2022-04-21 08:01:55 -0400

    fdsa

--- Test/Attempt3/Home.php
+++ Test/Attempt3/Home.php
@@ -125,9 +125,10 @@ function leaderboardRequest(){
   <div style = "text-align:center; position:relative; top:30px; font-size:48px; color:white;">
      Press Button to Play
   </div>
-
+  <div>
   <button type ="submit" onclick = "goToGame()" class="button button1">Play</button>
   <button type ="submit" onclick = "logOut()" class="button button2">Log out</button>
+  </div>
   <div style = "text-align:center; position:relative; top:30px; font-size:48px; color:white;">
      Leaderboard
   </div>
