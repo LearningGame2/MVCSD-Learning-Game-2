908fac3ecef745acb8888c7a90aa3b2acceab3e4
MasonFishell <fishell1@kenyon.edu>
2022-04-27 17:35:15 -0400

    fdsa

--- Test/Attempt3/Home.php
+++ Test/Attempt3/Home.php
@@ -3,9 +3,9 @@
 
 session_start();
 
-if(!isset($_COOKIE['Username'])){
-  header("location: http://cslab.kenyon.edu/class/ssd/Game2/LGAttempt3/Login1.php");
-} //Comment out to make less annoying
+// if(!isset($_COOKIE['Username'])){
+//   header("location: http://cslab.kenyon.edu/class/ssd/Game2/LGAttempt3/Login1.php");
+// } //Comment out to make less annoying
 
 
 
