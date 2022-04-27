50a4f4358c0e8d0e09e32414903cb6f87d83b301
dailey2 <dailey2@kenyon.edu>
2022-04-27 18:15:44 -0400

    Update Home.php

--- Test/Attempt3/Home.php
+++ Test/Attempt3/Home.php
@@ -114,6 +114,8 @@ function leaderboardRequest(){
       text-align:center;
       font-size:23px;
       font-family: "Lucida Console", "Courier New", monospace;
+      height:100px;
+      width:100px
     }
     .center {
       margin-left: auto;
