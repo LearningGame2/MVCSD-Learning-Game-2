70e22e86865ee8355f273bd7033e0ce171315007
dailey2 <dailey2@kenyon.edu>
2022-05-01 17:22:11 -0400

    summary

--- Test/Attempt3/Level4.php
+++ Test/Attempt3/Level4.php
@@ -54,10 +54,32 @@ function promptRequest($numOfQuestions){
   <title>
     Level 4
   </title>
+
+  <style>
+    body, html {
+      height: 100%;
+      margin: 0;
+    }
+
+    .bg {
+      /* The image used */
+      background-image: url("duck-meet-g84831808a_1920.jpg");
+
+      /* Full height */
+      height: 100%;
+
+      /* Center and scale the image nicely */
+      background-position: center;
+      background-repeat: no-repeat;
+      background-size: cover;
+    }
+  </style>
 </head>
 
+
 <body onload="NextQuestion(0)">
-  <h1 style="background-color:black; color:white; text-align:center; font-family:Garamond; font-size:300%; line-height: 100px">
+<div class="bg">
+  <h1 style="color:white; text-align:center; font-family:Garamond; font-size:300%; line-height: 100px">
     Level 4
   </h1>
     <main>
@@ -144,7 +166,7 @@ function promptRequest($numOfQuestions){
 
         </div>
     </main>
-
+</div>
 
 <script>
 
