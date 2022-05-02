889dbbec7dfd9855711efc08d23436b4f6b89efe
dailey2 <dailey2@kenyon.edu>
2022-05-02 15:28:04 -0400

    Update Home.php

--- Test/Attempt3/Home.php
+++ Test/Attempt3/Home.php
@@ -191,8 +191,8 @@ function leaderboardRequest(){
   </div>
 
   <div class="inLine" style = "text-align:center; position:absolute; top:300px; font-size:48px; color:white; font-family: 'Lucida Console', 'Courier New', monospace;">
-     <span style="right:15%"> Leaderboard </span>
-     <span style="right:85%"> Your Stats </span>
+     <span style="float:left"> Leaderboard </span>
+     <span style="float:right;"> Your Stats </span>
   </div>
 
   <table class="alignLeft inLine">
