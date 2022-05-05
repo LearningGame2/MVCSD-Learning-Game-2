225642f4cf5a254b8b268fe4772ea90f4fa5c20f
MasonFishell <fishell1@kenyon.edu>
2022-05-04 20:04:08 -0400

    fda

--- Test/Attempt3/Level5.php
+++ Test/Attempt3/Level5.php
@@ -333,7 +333,7 @@ function handleNextQuestion() {
                 handleEndGame()//ends game if index number greater than 4 meaning we're already at the 5 question
             }
             resetOptionBackground()
-        }, 2000);
+        }, 10);
 }
 
 //sets options background back to null after display the right/wrong colors
