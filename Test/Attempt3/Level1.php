bae54c67a0783a9c70ac23ddc8eec08c151e68e3
dailey2 <dailey2@kenyon.edu>
2022-05-06 15:32:43 -0400

    Update Level1.php

--- Test/Attempt3/Level1.php
+++ Test/Attempt3/Level1.php
@@ -99,7 +99,7 @@ function promptRequest($numOfQuestions){
 <body onload="NextQuestion(0)">
 
   <audio id="correctAnswerSound">
-    <source src="winning-bleeps.wav" type="audio/wav">
+    <source src="/winning-bleeps.wav" type="audio/wav">
     Your browser does not support the audio element.
   </audio>
 
