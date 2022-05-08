e334902e04ec55d4da1a003723d09c1aae20fe0a
dailey2 <dailey2@kenyon.edu>
2022-05-08 16:49:41 -0400

    Update Instructions.php

--- Test/Attempt3/Instructions.php
+++ Test/Attempt3/Instructions.php
@@ -4,6 +4,9 @@
 <title>Instructions</title>
 
 <style>
+    body{
+      margin:100px;
+    }
     .button {
       text-align: center;
       background-color: #197DDD;
@@ -21,13 +24,11 @@
       height:100px;
       width:400px;
       transition-duration: 0.4s;
-      border: 2px solid white;
     }
     .button1 {
       margin: 0;
       position:absolute;
       top:100px;
-      right:-100px;
       -ms-transform: translate(-50%, -50%);
       transform: translate(-50%, -50%);
     }
@@ -51,6 +52,10 @@
 </style>
 
 <body>
+    <div style="text-align:center;">
+        <button type="submit" onclick="returnHome()" class="button button1">Return Home!</button>
+    </div>
+
     <h1 class="heading">
         You're gonna want to read this...
     </h1>
@@ -65,47 +70,45 @@
         work hard!  You have to score well enough on each level before a game in order to play it.  We'll let you<br>
         find out how well...now go flex those mathematical muscles!
     </div>
-    
+
     <br><br>
-    <h2>Instructions</h2>
+
+    <h2>Instructions:</h2>
     <div class="how-to-play">
-
-            Packabunchas: fit the shapes into the box!
-            <ul>
-                <li>Double click on a shape to rotate (make sure you click hard on your Chromebooks!)
-                <li>Click and drag shapes into the box so that they cover all the empty squares and don't go outside of the box
-                <li>Tips: start with sections that only certain pieces could fill, if a piece doesn't work in an orientation,<br>
-                take them out and try each one by one, and keep at it!  They all have at least one solution.
-                <li>You receive a 3x multiplier for getting to this game — if you did well enough on rounding to reach it
-            </ul>
-            <br>
-            HexGL: keep your spaceship on track and don't crash...
-            <ul>
-                <li>Press up arrow key to increase engine power
-                <li>Use side arrow keys to navigate the course
-                <li>You only have a limited amount of health and you can't get it back, so be cautious rather than speedy!<br>
-                    Otherwise, you won't get a score multiplier
-            </ul>
-            <br>
-            Astronaut: You have one objective — survive in the vacuum of space.
-            <ul>
-                <li>Press space to jump, and not too early!
-                <li>Don't fall between the ships
-            </ul>
-            <br>
-            Duck Hunt: This one's a classic.  You know the drill.
-            <ul>
-                <li>Click to shoot the ducks
-                <li>Don't waste your anmo
-                <li>See if you can hit two with one shot!
-            </ul>
-            <button type="submit" onclick="phoneHome()" class="button button1">Take me back home!</button>
-
+        Packabunchas: fit the shapes into the box!
+        <ul>
+            <li>Double click on a shape to rotate (make sure you click hard on your Chromebooks!)
+            <li>Click and drag shapes into the box so that they cover all the empty squares and don't go outside of the box
+            <li>Tips: start with sections that only certain pieces could fill, if a piece doesn't work in an orientation,<br>
+            take them out and try each one by one, and keep at it!  They all have at least one solution.
+            <li>You receive a 3x multiplier for getting to this game — if you did well enough on rounding to reach it
+        </ul>
+        <br>
+        HexGL: keep your spaceship on track and don't crash...
+        <ul>
+            <li>Press up arrow key to increase engine power
+            <li>Use side arrow keys to navigate the course
+            <li>You only have a limited amount of health and you can't get it back, so be cautious rather than speedy!<br>
+                Otherwise, you won't get a score multiplier
+        </ul>
+        <br>
+        Astronaut: You have one objective — survive in the vacuum of space.
+        <ul>
+            <li>Press space to jump, and not too early!
+            <li>Don't fall between the ships
+        </ul>
+        <br>
+        Duck Hunt: This one's a classic.  You know the drill.
+        <ul>
+            <li>Click to shoot the ducks
+            <li>Don't waste your anmo
+            <li>See if you can hit two with one shot!
+        </ul>
     </div>
 </body>
 
 <script>
-    function phoneHome(){
+    function returnHome(){
         window.location.href="Home.php";
     }
 </script>
