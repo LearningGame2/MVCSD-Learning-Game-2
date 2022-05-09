54995dd9081f5a618a377b830c743f0b54e94de4
dailey2 <dailey2@kenyon.edu>
2022-05-09 12:53:20 -0400

    Update More.php

--- Test/Attempt3/More.php
+++ Test/Attempt3/More.php
@@ -1,15 +1,15 @@
 <!DOCTYPE html>
 <html lang = "en">
-    <head>
-        <link rel = "stylesheet">
-        <title>
-            About us!
-        </title>
-        <link rel="icon" type="image/x-icon" href="../LGAttempt3/LG2Images/KenyonLogo.ico">
-    </head>
 
-    <style>
+<head>
+    <link rel = "stylesheet">
+    <title>
+        About us
+    </title>
+    <link rel="icon" type="image/x-icon" href="../LGAttempt3/LG2Images/KenyonLogo.ico">
+</head>
 
+<style>
     .button {
       text-align: center;
       background-color: #197DDD;
@@ -40,66 +40,61 @@
     .button1:hover {
       background-color: green;
     }
-    .header{
-        margin-left: -80px;
-        position:relative;
-        margin-top:100px;
-    }
-    .infoP{
-        margin-right: 100px;
-        margin-left: 100px;
-    }
-    .creds{
-        margin-left: -35px;
-    }
     .games{
         margin-left: -35px;
         color: rgb(91, 75, 105);
     }
-    </style>
+</style>
 
-    <button type="submit" onclick="sendIt()" class="button button1">Take me home!</button>
+<body>
 
-    <canvas id="myCanvas" width="578" height="200">Canvas not supported in this browser</canvas>
-    <script src="wigginSt.js"></script>
+    <div style="text-align:center;">
+        <button type="submit" onclick="returnHome()" class="button button1">Return Home!</button>
+    </div>
 
-    <h1 class="header">
-        Some background info on this project: <br><br>
-    </h1>
+    <div>
+        <h1>Some background info on this project:</h1>
+    </div>
 
-    <body id="b1" class="infoP">
-        This website was produced by 3 students of Kenyon College: Connor Dailey, Mason Fishell, and Sam Rabieh, for the class Software 
-        and Systems Design with Professor James Skon.  It is a collaboration with Wiggin St. Elementary School, created for the learning 
-        and enjoyment of Ms. Brenneman's fifth grade class.  Most Tuesdays of February — March 2022, we came in and discussed programming, 
-        what's important in good game design, and what questions we should actually put in the game. <br><br>
-    </body>
+    <div id="b1">
+      <p>
+        This website was produced by 3 students of Kenyon College: Connor Dailey, Mason Fishell, and Sam Rabieh, for the class Software
+        and Systems Design with Professor James Skon.  It is a collaboration with Wiggin St. Elementary School, created for the learning
+        and enjoyment of Ms. Brenneman's fifth grade class.  Most Tuesdays of February — March 2022, we came in and discussed programming,
+        what's important in good game design, and what questions we should actually put in the game.
+      <p>
+    </div>
 
-    <body id="b2" class="infoP">
-        However, this would not have been possible coding entirely on our own from the ground up.  We did a lot of hard work putting together 
-        the different parts of our project — and believe us, there are a lot of parts to it — and we wrote the code for the actual website all 
-        by ourselves, but we implemented the structure of the quiz and the minigames from open-source code we found elsewhere online.  We 
-        found the code for a quiz game and started working with it just to learn a few things, and both we and the students really liked it 
-        so we stuck with that design.  A few weeks before the end of the semester, we finally got our MySQL database to connect with the 
-        website to allow users to log in and get randomized questions through the phpMyAdmin interface.  Then it was time to decide how we 
-        could make it better and more fun to use.  We don't have much experience working with graphics, so we didn't want to waste time trying 
-        to build a game ourselves from scratch, especially if it might not be that good in the end.  We implemented 4 games to keep it fun in 
-        between the levels of the quiz, and we think we succeeded.  Enjoy!<br><br><br>
-    </body>
+    <div id="b2">
+      <p>
+        However, this would not have been possible coding entirely on our own from the ground up.  We did a lot of hard work putting together
+        the different parts of our project — and believe us, there are a lot of parts to it — and we wrote the code for the actual website all
+        by ourselves, but we implemented the structure of the quiz and the minigames from open-source code we found elsewhere online.  We
+        found the code for a quiz game and started working with it just to learn a few things, and both we and the students really liked it
+        so we stuck with that design.  A few weeks before the end of the semester, we finally got our MySQL database to connect with the
+        website to allow users to log in and get randomized questions through the phpMyAdmin interface.  Then it was time to decide how we
+        could make it better and more fun to use.  We don't have much experience working with graphics, so we didn't want to waste time trying
+        to build a game ourselves from scratch, especially if it might not be that good in the end.  We implemented 4 games to keep it fun in
+        between the levels of the quiz, and we think we succeeded.  Enjoy!
+      </p>
+    </div>
 
-    <body>
-        <div class="creds">Credits:</div>
+    <div>
+        <h1>Credits:</h1>
         <ul class="games">
             <li>Packabunchas, a colorful puzzle game by Mattia Fortunati
             <li>Astronaut in Trouble, a simple pixel side-scroller by Douglas Lopes
             <li>HexGL, a futuristic HTML5 racing game by BKCore / Thibault Despoulain
             <li>Duck Hunt, a classic Nintendo game remade for the web browser experience by Matt Surabian
         </ul>
-    </body>
+    </div>
 
-    <script>
-        function sendIt(){
-            window.location.href="Home.php";
-        }
-    </script>
+</body>
+
+<script>
+    function returnHome(){
+        window.location.href="Home.php";
+    }
+</script>
 
 </html>
