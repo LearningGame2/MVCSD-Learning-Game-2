8c45307aa93571af3097cb3ce13465b0eec8aff9
srabieh <rabieh1@kenyon.edu>
2022-05-11 15:00:42 -0400

    this better work!  it did for set of 10 questions!

--- Test/Attempt3/Level5.php
+++ Test/Attempt3/Level5.php
@@ -25,7 +25,7 @@ function connect() {
 function promptRequest($numOfQuestions){
     $questions = array();
     for ($x = 0; $x <= $numOfQuestions; $x++) {
-        $rowNumber = rand(1550,1610);
+        $rowNumber = rand(1400,1799);
         $prompt = "";
         $conn = connect();
         $sql = "SELECT * FROM QuestionDatabase WHERE QuestionNumber = '$rowNumber'";
