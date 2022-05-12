0c15c52863483c1230694d3fdc0750c310be3363
dailey2 <dailey2@kenyon.edu>
2022-05-12 00:24:19 -0400

    Update Home.php

--- Test/Attempt3/Home.php
+++ Test/Attempt3/Home.php
@@ -80,7 +80,7 @@ function leaderboardRequest(){
     }
     .button {
       text-align: center;
-      background-color:#CD5C5C;
+      background-color:#FFDF00;
       border-radius: 12px;
       color: white;
       padding: 15px 32px;
